854be9734ed2154df31fe3277d9510a0b2d0c2ed
Georgi <gk.manev@gmail.com>
2023-01-19 14:01:03 +0200

    up

--- project/forecast/settings.py
+++ project/forecast/settings.py
@@ -111,7 +111,7 @@ CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis:6379/0")
 CELERY_BEAT_SCHEDULE = {
     
       'task_rq_api': {
-        'task': 'forecast.tasks.sample_task',
+        'task': 'tips.tasks.sample_task',
         'schedule': crontab(hour=13, minute=54),
     }
    
